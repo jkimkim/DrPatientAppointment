e5d7d3716f053e7bfa073f36811e0de1d037f234
JOHN <65081425+jkimkim@users.noreply.github.com>
2023-05-03 16:06:38 +0300

    update

--- app/src/main/java/app/web/jkimtech/drpatientappointment/controller/DoctorProfileActivity.java
+++ app/src/main/java/app/web/jkimtech/drpatientappointment/controller/DoctorProfileActivity.java
@@ -5,12 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import app.web.jkimtech.drpatientappointment.R;
+import io.alterac.blurkit.BlurLayout;
 
 public class DoctorProfileActivity extends AppCompatActivity {
+    //blurkit library
+    BlurLayout blurLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_profile);
+        blurLayout = findViewById(R.id.blur_layout);
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        blurLayout.startBlur();
+    }
+
+    @Override
+    protected void onStop() {
+        blurLayout.pauseBlur();
+        super.onStop();
     }
 }
